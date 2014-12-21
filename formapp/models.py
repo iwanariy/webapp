d16d4889f1eb29e3a9f7890642f5952caad7021d
narikin <narikin.github@gmail.com>
2014-12-21 21:55:49 +0900

    Modify model

--- formapp/models.py
+++ formapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(u"Name", max_length=256)
+    mail_address = models.CharField(u"Mail Address", max_length=256)
     title = models.CharField(u"Title", max_length=256)
     description = models.TextField(u"Description")
-    mail_address = models.CharField(u"Mail Address", max_length=256)
 
     def __unicode__(self):
         return self.item_code
