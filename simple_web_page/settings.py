e6e234cecc56a50684486b7e8bffe1ffe3ff73ed
narikin <narikin.github@gmail.com>
2014-12-21 22:43:43 +0900

    Add encoding and PROJECT_DIR.

--- simple_web_page/settings.py
+++ simple_web_page/settings.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 Django settings for simple_web_page project.
 
@@ -11,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
