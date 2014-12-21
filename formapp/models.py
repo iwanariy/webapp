41a9450515a9881b70031e918605814d24649f33
narikin <narikin.github@gmail.com>
2014-12-21 21:53:39 +0900

    Define Contact model.

--- formapp/models.py
+++ formapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(u"Name", max_length=256)
+    title = models.CharField(u"Title", max_length=256)
+    description = models.TextField(u"Description")
+    mail_address = models.CharField(u"Mail Address", max_length=256)
+
+    def __unicode__(self):
+        return self.item_code
+
+    class Meta:
+        db_table = 'contact'
+        verbose_name = u"お問い合わせ"
+        vervose_name_plural = u"お問い合わせ"
