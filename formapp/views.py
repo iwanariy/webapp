6a6fc7f628e16d2a1d21f396388e400e9a15f318
narikin <narikin.github@gmail.com>
2014-12-23 03:34:44 +0900

    Implement save functionality.

--- formapp/views.py
+++ formapp/views.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from formapp.forms import ContactForm
 
 
 def contact_display(request):
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('contact_complete/')
+    else:
+        form = ContactForm()
 
     return render(request, 'contact_display.html', {'form': form})
 
