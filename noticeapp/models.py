42849acad93b834312529c5cf1474a0f5b2306c1
narikin <narikin.github@gmail.com>
2014-12-29 23:30:53 +0900

    Define Notice model.

--- noticeapp/models.py
+++ noticeapp/models.py
@@ -1,3 +1,20 @@
+# encoding: utf-8
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Notice(models.Model):
+    title = models.CharField(u"件名", max_length=256)
+    description = models.TextField(u"内容")
+    author = models.CharField(u"作成者", max_length=256)
+    published_at = models.DateTimeField(u"発行日", default=datetime.now())
+    created_at = models.DateTimeField(u"作成日", default=datetime.now())
+    is_published = models.BooleanField(u"発行済", default=False)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'notices'
+        verbose_name = u"お知らせ"
+        verbose_name_plural = u"お知らせ"
