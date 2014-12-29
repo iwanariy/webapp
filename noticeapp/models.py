d2fcd3feb4abb2d740391ff928476f55e20cced0
narikin <narikin.github@gmail.com>
2014-12-30 01:28:01 +0900

    Modify default value.

--- noticeapp/models.py
+++ noticeapp/models.py
@@ -7,7 +7,7 @@ class Notice(models.Model):
     title = models.CharField(u"件名", max_length=256)
     description = models.TextField(u"内容")
     author = models.CharField(u"作成者", max_length=256)
-    published_at = models.DateTimeField(u"発行日", default=datetime.now())
+    published_at = models.DateTimeField(u"発行日")
     created_at = models.DateTimeField(u"作成日", default=datetime.now())
     is_published = models.BooleanField(u"発行済", default=False)
 
