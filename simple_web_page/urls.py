65c43df55bfd862d9b8e7a399d05a500190cb553
narikin <narikin.github@gmail.com>
2014-12-30 22:28:00 +0900

    Add url setting for static images.

--- simple_web_page/urls.py
+++ simple_web_page/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 urlpatterns = patterns(
     '',
@@ -11,4 +13,4 @@ urlpatterns = patterns(
     url(r'^contact/$', 'formapp.views.contact_display'),
     url(r'^contact/contact_complete/', 'formapp.views.contact_complete'),
     url(r'^notice/$', 'noticeapp.views.index'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
