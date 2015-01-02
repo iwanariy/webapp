333e14d9a482b7ab1c5d48e1a819a75f0a3947d8
narikin <yuki.iwnr@gmail.com>
2015-01-02 17:47:51 +0900

    #5 formappのURLConfをformappに移動

--- simple_web_page/urls.py
+++ simple_web_page/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+admin.autodiscover()
 
 urlpatterns = patterns(
     '',
@@ -10,8 +11,7 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^contact/$', 'formapp.views.contact_display'),
-    url(r'^contact/contact_complete/', 'formapp.views.contact_complete'),
+    url(r'^contact/', include('formapp.urls')),
     url(r'^notice/$', 'noticeapp.views.index'),
     url(r'^image/$', 'imageapp.views.index'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
