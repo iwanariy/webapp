5d501a486c41a601098f42ea7825258740feb642
narikin <yuki.iwnr@gmail.com>
2015-03-30 00:39:59 +0900

    Refactor templates dir for noticeapp

--- noticeapp/views.py
+++ noticeapp/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render
 from noticeapp.models import Notice
 
+APP_NAME = 'noticeapp'
+
 
 # GET /xxx/
 def index(request):
     notices = Notice.objects.all()
-    return render(request, 'index.html', {'notices': notices})
+    return render(request, '%s/index.html' % APP_NAME, {'notices': notices})
